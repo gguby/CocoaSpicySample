a07529599ff4626f03ca425b3eee6ed0c355c3a9
정원식(Wonsik Jung)/Media솔루션사업팀/SKP <gguby@sk.com>
2022-07-08 15:02:43 +0900

    first init

---
+++ CocoaSpicySample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoaSpicySample
+//
+//  Created by wsjung on 2022/07/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ framework/GStreamer.framework/Versions/Current/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/matt/Projects/cerbero-1.20/build/dist/darwin_universal/arm64/lib"
+XML2_LIBS="-lxml2 -L/Users/matt/Projects/cerbero-1.20/build/dist/darwin_universal/arm64/lib -lz  -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/matt/Projects/cerbero-1.20/build/dist/darwin_universal/arm64/include/libxml2"
+MODULE_VERSION="xml2-2.9.9"
+
